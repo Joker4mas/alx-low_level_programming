5dfc749851ce50e5011ac1c9a6234faa8c7db0c0
Joker4mas <samuelogbaje@gmail.com>
2023-03-18 05:48:07 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,12 +5,12 @@
  *
  * printf: it displays to the screen
  *
- * Return : Always 0 (Success)
+ * Return:Always 0 (Success)
  */
 
-int main(void)
+int main(voidi)
 
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
