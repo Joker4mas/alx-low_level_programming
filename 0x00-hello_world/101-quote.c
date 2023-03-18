1bba4855deff198dee474283422a39b3978d0268
Joker4mas <samuelogbaje@gmail.com>
2023-03-18 07:38:17 -0700

    unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point
+ *
+ * Return: always 1 (Success)
+ */
+
+int main(void)
+{
+	fprintf(stderr,"and that piece of art is useful" - Dora Korpar, 2015-10-1\n");
+	return(1);
+}
