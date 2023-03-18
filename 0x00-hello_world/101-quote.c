69c95cf9977c5b0cc147586d987cb6401062bebd
Joker4mas <samuelogbaje@gmail.com>
2023-03-18 07:56:00 -0700

    unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	fprintf(stderr,"and that piece of art is useful" - Dora Korpar, 2015-10-1\n");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-1\n,");
 	return(1);
 }
