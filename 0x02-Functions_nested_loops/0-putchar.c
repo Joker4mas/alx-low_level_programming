5b80a418f3c0290ac34e83ff523bbf924f04b808
Joker4mas <samuelogbaje@gmail.com>
2023-04-07 11:10:19 -0700

    putchar

--- 0x02-Functions_nested_loops/0-putchar.c
+++ 0x02-Functions_nested_loops/0-putchar.c
@@ -1,13 +1,21 @@
-#include <stdio.h>
+#includ "main.h"
 
 /**
  * main - Entry point
  *
- * Return - 0 (success)
+ * Return - Always 0 (success)
  */
 
-int main()
+int main(void)
 {
-	printf("_putcha\n");
+	char  str[] = "_putchar";
+	int n;
+	int getlen = strlen(str);
+
+	for ( n=0; n<getlen; n++)
+	{
+		_putchar(str[n]);
+	}
+	_putchar('\n');
 	return (0);
 }
