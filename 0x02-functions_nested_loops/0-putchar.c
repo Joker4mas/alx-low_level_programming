c225e31f69b202dee168d9f54a4f409149a8019c
Joker4mas <samuelogbaje@gmail.com>
2023-04-08 07:38:00 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -11,11 +11,12 @@ int main(void)
 	char str[] = "_putchar";
 	int n;
 	int getlen = strlen(str);
-	for (n = 0; n < getlen; n++)
 
+	for (n = 0; n < getlen; n++)
         {
-	_putchar(str[n]);
+		_putchar(str[n]);
         }
+
        	_putchar('\n') ;
         return (0);
 }
