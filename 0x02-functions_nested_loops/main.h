dd243d34d24d62030b6e9933a7ade2acb332e160
Joker4mas <samuelogbaje@gmail.com>
2023-04-08 07:07:16 -0700

    putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,18 @@
+#include <unstd.h>
+#include <stdlib.h>
+#include <math.h>
+#include <ctype.h>
+#include <string.h>
+
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet_x10(void);
+int _islower(int c);
+int is _alpha(int c);
+int print_sign(int n);
+int _abs(int);
+int print_last_digit(int);
+void time_table(void);
+void jack_bauer(void);
+int add(int a, int b);
+void print_to_98(int n);
