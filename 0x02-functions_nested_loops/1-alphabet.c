06dd19b8fee6fe264e1e2d5f60f941beef9bfc4f
Joker4mas <samuelogbaje@gmail.com>
2023-04-08 11:01:18 -0700

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - Main entry point
+ *
+ * Return: Always 0 (success)
+ *
+ */
+
+void print_alphabet(void)
+{
+	char n ;
+	for (n = 'A'; n < 'Z'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
