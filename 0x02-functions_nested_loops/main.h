91909d12c8283998763274fba2e1b922c19ee74c
Joker4mas <samuelogbaje@gmail.com>
2023-04-09 08:59:42 -0700

    time_table

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,7 +12,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
-void time_table(void);
+void times_table(void);
 void jack_bauer(void);
 int add(int a, int b);
 void print_to_98(int n);
