42243c895c22d4fe8ce13e887691f4b4e0a14405
Joker4mas <samuelogbaje@gmail.com>
2023-04-09 09:22:06 -0700

    10-add

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,11 +2,15 @@
 
 /**
  * main - function that adds two numbers
+ * 
+ * @x: is a parameter used.
+ *
+ * @y: is a parameter used.
  *
  * Return: Always 0 (success).
  */
 
-int add(int x,int y)
+int add(int x, int y)
 {
 	return (x + y);
 }
