c3868285c31d954bfe243a94a37dff4bce507217
Joker4mas <samuelogbaje@gmail.com>
2023-04-09 08:49:49 -0700

    Jack bauer

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,9 +9,9 @@
 
 void jack_bauer(void)
 {
-	int x,y;
-	
-	for (x = 0; x <= 24; x++)
+	int x, y;
+
+	for (x = 0; x <= 23; x++)
 	{
 		for (y = 0; y <= 59; y++)
 		{
@@ -22,6 +22,6 @@ void jack_bauer(void)
 			_putchar (y % 10 + '0');
 			_putchar ('\n');
 		}
-	}	
+	}
 
 }
