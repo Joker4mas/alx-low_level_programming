f833300a6edb4fff5fc09cf90d2f412000b4cf24
Joker4mas <samuelogbaje@gmail.com>
2023-04-09 08:56:45 -0700

    time_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,12 +8,12 @@
 
 void times_table(void)
 {
-	int x,y,product;
+	int x, y, product;
 	
 	for (x = 0; x <=9; x++)
 	{
 		_putchar(48);
-		for (y = 0; y <= 9; y++)
+		for (y = 1; y <= 9; y++)
 		{
 			_putchar(',');
 			_putchar(' ');
