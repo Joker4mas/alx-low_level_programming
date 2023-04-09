aff9e43407f26b2b94ae8ffc387d4f8cdb6ff51d
Joker4mas <samuelogbaje@gmail.com>
2023-04-09 08:53:54 -0700

    time_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * time_table - function that prints time table
  *
  * Return: Always 0 (success).
  */
@@ -23,7 +23,7 @@ void times_table(void)
 			if (product <= 9)
 				_putchar(' ');
 			else
-				putchar((product / 10) + 48);
+				_putchar((product / 10) + 48);
 			_putchar((product % 10) + 48);
 		}
 		_putchar('\n');
