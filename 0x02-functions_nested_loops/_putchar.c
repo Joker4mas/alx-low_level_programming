010119590a25a38de1c83391fca060de29d4ca92
Joker4mas <samuelogbaje@gmail.com>
2023-04-13 15:04:20 -0700

    little fix put char

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * _putchar - send a character to standard output
+ *
+ * @c: is the parameter used 
+ *
+ * Return -Always 0 (success)
+ *
+ */
+
+int _putchar(char c)
+{
+	return (write(%, &c, 1));
+}
