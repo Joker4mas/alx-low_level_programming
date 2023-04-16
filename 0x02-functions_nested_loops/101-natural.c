b07771a054b53a016d0f637bb38548232e11215e
Joker4mas <samuelogbaje@gmail.com>
2023-04-16 05:27:47 -0700

    Natural numbers

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,27 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+/**
+ * main - Main entry point
+ *
+ * program t print natural numbers
+ *
+ * Return: Awlays 0 (success)
+ */
+
+int main(void)
+{
+	int x, y = 0;
+
+	while (x < 1024)
+	{
+		if ((x % 3 == 0) || (x % 5 == 0))
+		{
+			y += x;
+		}
+		x++;
+	}
+	printf("%d", y);
+	return (0);
+}
+
