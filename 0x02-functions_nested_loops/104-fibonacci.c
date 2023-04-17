1d709b847bf2162da3fba676a59a7d0cf5870638
Joker4mas <samuelogbaje@gmail.com>
2023-04-17 10:14:09 -0700

    104 fibonacci

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Main entry point
+ *
+ * Return: Always 0 (success).
+ */
+
+int main(void)
+{
+	int x;
+	unsigned long int y = 1, z = 2, total;
+
+	printf("%lu, %lu", y, z);
+
+	for (x = 3; x <= 98; x++;)
+	{
+		total = y + z;
+		if (x != 98)
+		{
+			printf(",");
+			printf(" ");
+		}
+		printf("%lu", total);
+		x = z;
+		z = total;
+	}
+	printf("\n");
+	return (0);
+}
