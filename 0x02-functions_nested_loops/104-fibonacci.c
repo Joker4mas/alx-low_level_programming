61f342c59d2718e419eea95e9940cae21b6bd5da
Joker4mas <samuelogbaje@gmail.com>
2023-04-18 00:58:28 -0700

    104-fibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -13,10 +13,10 @@ int main(void)
 
 	printf("%lu, %lu", y, z);
 
-	for (x = 0; x <= 98; x++)
+	for (x = 3; x <= 97; x++)
 	{
 		total = y + z;
-		if (x != 98)
+		if (x != 97)
 		{
 			printf(",");
 			printf(" ");
