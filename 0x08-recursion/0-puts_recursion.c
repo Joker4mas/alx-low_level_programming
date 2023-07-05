25bd0176946c70f8e9cb680db5733b24d6ca8086
Joker4mas <samuelogbaje@gmail.com>
2023-07-05 06:43:54 -0700

    recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _puts_recursion- function to print a string.
+ *
+ * Return: returns 0 (success)
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+        if (*s != '\0')
+        {
+                _putchar(*s);
+                _puts_recursion(s + 1);
+        }
+        esle
+                _putchar('\n');
+}
+
