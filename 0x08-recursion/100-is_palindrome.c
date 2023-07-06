512632ad2950b713167045becd83adbd299dfffd
Joker4mas <samuelogbaje@gmail.com>
2023-07-06 03:26:31 -0700

    fix error on palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -3,7 +3,7 @@
 
 int validate_palind(char *s, int x1, int x2);
 /**
- * validate_palind - function the output 1 if a string 
+ * is_palindrome - function the output 1 if a string
  * is palindrome and 0 if it is not a palindrome.
  * @s: string
  * Return: Always  0 (succes)
@@ -14,8 +14,8 @@ int is_palindrome(char *s)
 	return (validate_palind(s, 0, strlen(s) - 1));
 }
 
-/** 
-* is_palindrome - function the output 1 if a string  
+/**
+* validate_palind - function the output 1 if a string 
 * is palindrome and 0 if it is not a palindrome.
 * @s: string
 * @x1: start
@@ -29,7 +29,7 @@ int validate_palind(char *s, int x1, int x2)
 		return (1);
 	else if (s[x1] != s[x2])
 		return (0);
-	else 
+	else
 		return (validate_palind(s, x1 + 1, x2 - 1));
 }
 
